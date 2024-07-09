efa3d3cedbce56d532bdcd34e63b2d6cdf307dae
alex <alexxpyykonen@gmail.com>
2024-07-09 05:13:07 +0300

    add ed the group adding logic to the script

--- install_docker.sh
+++ install_docker.sh
@@ -27,13 +27,13 @@ sudo apt-get install -y docker-ce
 
 # Install Docker Compose
 sudo curl -L "https://github.com/docker/compose/releases/download/v2.13.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
 
-# Enable and start Docker service
-sudo systemctl enable docker
-sudo systemctl start docker
 
-# Verify installation
+sudo usermod -aG docker $USER
+newgrp docker
+
 docker --version
 docker compose version
 
+# Print message to log out and back in
+echo "Please log out and log back in to apply Docker group membership"
